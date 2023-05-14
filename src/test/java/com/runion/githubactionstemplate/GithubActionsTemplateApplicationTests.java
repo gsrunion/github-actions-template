8c16f2a2a0bc63a165e6c7de4e4541bd1878a4f3
gsrunion <gsrunion@gmail.com>
2023-05-14 11:20:05 -0400

    Initializing with content from Spring Initializer

---
+++ src/test/java/com/runion/githubactionstemplate/GithubActionsTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runion.githubactionstemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
